f9a9fd2340700f114878199a11a3a22a5f6d7ef8
Sushan <sushanrs15@gmail.com>
2024-11-27 09:46:30 +0530

    Employee-performance-report-angular-part

---
+++ .angular/cache/19.0.1/employee-management-system/vite/deps_ssr/@angular_ssr.js
@@ -0,0 +1,36 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  AngularAppEngine,
+  InlineCriticalCssProcessor,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  destroyAngularServerApp,
+  extractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig,
+  provideServerRoutesConfig,
+  setAngularAppEngineManifest,
+  setAngularAppManifest
+} from "./chunk-MLPVE246.js";
+import "./chunk-ELBNUTW6.js";
+import "./chunk-LPIUXFST.js";
+import "./chunk-BJQ5X7NS.js";
+import "./chunk-D4D5G3JJ.js";
+import "./chunk-M56QPFQT.js";
+import "./chunk-YHCV7DAQ.js";
+export {
+  AngularAppEngine,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  provideServerRoutesConfig,
+  InlineCriticalCssProcessor as ɵInlineCriticalCssProcessor,
+  destroyAngularServerApp as ɵdestroyAngularServerApp,
+  extractRoutesAndCreateRouteTree as ɵextractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp as ɵgetOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig as ɵgetRoutesFromAngularRouterConfig,
+  setAngularAppEngineManifest as ɵsetAngularAppEngineManifest,
+  setAngularAppManifest as ɵsetAngularAppManifest
+};
+//# sourceMappingURL=@angular_ssr.js.map
